4b1a8ece32a67ac93c69073792e0232f8d28cf61
AH56k <99696128+Armen-Hammer@users.noreply.github.com>
2022-04-11 20:43:44 -0400

    Fixed a bug in the format for the date that showed the month instead of minute. Also added IPs to not send messages for, and the IP adress where the data came from in the text message

--- app.py
+++ app.py
@@ -19,12 +19,17 @@ def log():
     """This will redirect to an image of a 1x1 transparent pixel. First it will send me message by text about the email that was opened before redirecting"""
     description = request.args.get("desc")
 
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+
+    if ip_addr in os.environ.get("protected_IPs", []): #useful for not sending messages if the request comes from a specific IP (eg... your own adress)
+            return redirect("/static/1x1.png")
+
     message = client.messages.create(
             to=os.environ['to_number'], 
             from_=os.environ['from_number'],
-            body=f"Email opened at {datetime.datetime.now().strftime('%m/%d/%Y %H:%m')}. Description: {description}")
-    return redirect("/static/1x1.png")
+            body=f"Email opened at {datetime.datetime.now().strftime('%m/%d/%Y %H:%M')}. Description: {description}. IP: {ip_addr}")
 
+    return redirect("/static/1x1.png")
 
 
 if __name__ == "__main__":
